10c3a35bff26234f1c4cd49a459dcd96f7cd404a
Andrei Fedotov <anddfedotov@gmail.com>
2019-09-28 15:05:20 -0700

    Update config to include GoodReadsApiKey and DB URI

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 class Config(object):
     DEBUG = False
-    SECRET_KEY = 'test'
+    SECRET_KEY = 'secretkey'
+    GOODREADS_API_KEY = 'YoutGoodReadsAPIKey'
+
 
 class ProductionConfig(Config):
     ENV = 'production'
-    DATABASE_URI = ''
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'YourDatabaseURI'
+
 
 class DevelopmentConfig(Config):
     ENV = 'development'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:test@localhost:5432/bookworm'
     DEBUG = True
