e2b2b15ef41441a02214bad3a85ab0b6bc3b1378
Andrei Fedotov <anddfedotov@gmail.com>
2019-10-19 16:58:23 -0700

    Update config: disable sqlalchemy log

--- config.py
+++ config.py
@@ -8,9 +8,11 @@ class ProductionConfig(Config):
     ENV = 'production'
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'YourDatabaseURI'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     ENV = 'development'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:test@localhost:5432/bookworm'
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
